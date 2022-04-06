488505ee172c3f6c0d1d28112dbeaf443d850690
Jamuna Chavadi <102353993+01FE21MVE003@users.noreply.github.com>
2022-04-06 10:59:01 +0530

    Add files via upload

---
+++ MULTIPLICATION USING REPEATED ADDITION/PIPO1.v
@@ -0,0 +1,27 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date:    09:21:47 03/15/2022 
+// Design Name: 
+// Module Name:    PIPO1 
+// Project Name: 
+// Target Devices: 
+// Tool versions: 
+// Description: 
+//
+// Dependencies: 
+//
+// Revision: 
+// Revision 0.01 - File Created
+// Additional Comments: 
+//
+//////////////////////////////////////////////////////////////////////////////////
+module PIPO1(dout,din,ld,clk);
+input [15:0] din;
+input ld,clk;
+output reg [15:0] dout;
+always @(posedge clk)
+if(ld) dout<=din;
+endmodule
